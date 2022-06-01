d4a8b764ff00202d5e6bbd03d04a96458e11452b
chenqi0342 <1105458427@qq.com>
2022-06-01 19:34:01 +0800

    second commit

---
+++ forEach,for in,for-of.js
@@ -0,0 +1,10 @@
+//forEach是数组的方法
+//遍历的时候更加简洁，效率和for循环相同，不用关心集合index的问题
+//没有返回值
+//不能使用break中断循环，不能使用return返回到外层函数
+
+//for of 一个数据结构只要部署了Symbol.iterator属性，就被视为具有iterator接口
+//for of遍历：数组 Set Map,类数组
+
+//for in 用于遍历对象属性
+
